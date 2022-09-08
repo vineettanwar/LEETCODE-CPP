b1bc8e41dd137394156b2b7889405f18c36d1c74
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-09-09 00:40:02 +0530

    Time: 201 ms (47.21%), Space: 34.7 MB (63.57%) - LeetHub

--- 874-walking-robot-simulation/874-walking-robot-simulation.cpp
+++ 874-walking-robot-simulation/874-walking-robot-simulation.cpp
@@ -20,25 +20,21 @@ public:
         int res=0;
         for(auto &c:commands){
             if(c>0){
-                while(c-- && obst.count(to_string(x+dir[d][0])+":"+to_string(y+dir[d][1]))==0){
-                    x=x+dir[d][0];
-                    y=y+dir[d][1];
-                    // int newx=x+dir[d][0];
-                    // int newy=y+dir[d][1];
-                    // if(obst.count(to_string(newx)+":"+to_string(newy))==0){
-                    //     x=newx;
-                    //     y=newy;
-                    // }
-                res=max(res,x*x+y*y);
+                while(c--){
+                    int newx=x+dir[d][0];
+                    int newy=y+dir[d][1];
+                    if(obst.count(to_string(newx)+":"+to_string(newy))==0){
+                        x=newx;
+                        y=newy;
+                    }
                 }
-                    
-                
+                 res=max(res,x*x+y*y);
             }
-            
+           
             else if(c==-1)d=(d+1)%4;
             else if(c==-2)d=(d+3)%4;
             
         }
         return res;
-    }
+  }
 };
