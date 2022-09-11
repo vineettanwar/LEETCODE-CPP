131ee0be28eacd90bfd5363ace07906ae9596314
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-09-11 18:20:34 +0530

    Time: 202 ms (18.31%), Space: 36.9 MB (34.51%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -5,23 +5,29 @@ public:
         
         priority_queue<int,vector<int>,greater<int>> pq;
         
+        //bind speed and efficiency of engineer together
         for(int i=0;i<n;i++){
             v.push_back({efficiency[i],speed[i]});
         }
         
         long sum=0,ans=0;
         
+        //sort engineer on basis of efficiency 
         sort(v.begin(),v.end());
         
+        //start from end maximum efficiency 
         for(auto i=n-1;i>=0;i--){
             sum+=v[i].second;
             pq.push(v[i].second);
+            //if > k remove the engineer with smallest speed so that change is minimum
             if(pq.size()>k){
                 sum-=pq.top();
                 pq.pop();
             }
+            //calculate answer for ATMOST k engineer(MEANS WE CAN TAKE LESS THAN K IN TEAM).
             ans=max(ans,sum*v[i].first);
         }
+        //RETURN ANSWER
         return ans%1000000007;
         
     }
