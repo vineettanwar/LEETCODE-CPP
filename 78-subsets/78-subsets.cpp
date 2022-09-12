a6cec7e06f5852d7474f363f6bc28be0135fb58a
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-09-13 04:56:41 +0530

    Time: 8 ms (26.90%), Space: 6.9 MB (96.08%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
+       
         vector<vector<int>> res;
+        // 0 to 2^n-1
         for(int num=0;num<=(1<<n)-1;num++){
             vector<int> ans;
-            for(int j=0;j<n;j++){
-                if(num & (1<<j)){
-                    ans.push_back(nums[j]);
+            // 0 to nums length -1
+            for(int i=0;i<n;i++){
+                //if ith bit set take that number 
+                if(num & (1<<i)){
+                    ans.push_back(nums[i]);
                 }
             }
+            // ans after iterating all bits 
             res.push_back(ans);
         }
         return res;
