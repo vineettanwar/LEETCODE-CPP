d3636541111d36102289ba55d37e4394ac2d75a3
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-09-16 00:52:41 +0530

    Time: 181 ms (63.28%), Space: 58 MB (80.94%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        // int n =arr.size();
+        // // vector<int>ans;
+        // if(n%2){
+        //     return ans;
+        // }
+        unordered_map<int,int> mp;
+        for(auto x:arr){
+            mp[x]++;
+        }
+        sort(arr.begin(),arr.end());
+        int count=0;
+        for(auto x:arr){
+            if(mp[x]==0){
+                continue;
+            }
+            if(x>=0){
+                if(mp[x*2]==0){
+                    return false;
+                }
+                if(mp[x]>0 && mp[x*2] >0){
+                    count++;
+                    mp[x]--;
+                    mp[x*2]--;
+                }
+            }
+            else{
+                if(x%2!=0 || mp[x/2]==0){
+                    return false;
+                }
+                if(mp[x]>0 && mp[x/2] >0){
+                    count++;
+                    mp[x]--;
+                    mp[x/2]--;
+                }
+                
+            }
+        }
+        // for(auto x:mp){
+        //     if(x.second>0){
+        //         return false;
+        //     }
+        // }
+        return count==arr.size()/2;        
+    }   
+};
