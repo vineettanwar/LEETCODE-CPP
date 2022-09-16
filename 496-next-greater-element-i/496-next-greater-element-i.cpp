efc435bd9566abb9a15fe5f252e244be8a1aed85
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-09-16 23:43:35 +0530

    Time: 13 ms (41.21%), Space: 8.7 MB (70.15%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> s;
         vector<int> ans(nums2.size(),-1);
+        // prepare ans array of next greater element from nums 2
         for(int i=0;i<nums2.size();i++){
             while(!s.empty() && nums2[s.top()]<nums2[i]){
                 ans[s.top()]=nums2[i];
@@ -10,13 +11,17 @@ public:
             }
             s.push(i);
         }
+       // store indexes of nums2 
         unordered_map<int,int> mp;
         for(int i=0;i<nums2.size();i++){
             mp[nums2[i]]=i;
         }
-        for(auto x: ans){
-            cout<<x<<endl;
-        }
+        // for(auto x: ans){
+        //     cout<<x<<endl;
+        // }
+        
+        
+        // get indexes of element in in num2 and their corresponding next greater element
         vector<int> fnlans;
         for(int i=0;i<nums1.size();i++){
             int idx=mp[nums1[i]];
