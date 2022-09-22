76ecaa7ac24638933b6f86d18d2c469c1ffd7781
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-09-23 01:55:37 +0530

    Time: 587 ms (5.03%), Space: 305.9 MB (5.07%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,19 +1,28 @@
 class Solution {
+    string reverse(string s){
+        int st=0;
+        int e=s.length()-1;
+        while(st<=e){
+            swap(s[st++],s[e--]);
+        }
+        return s;
+    }
 public:
     string reverseWords(string s) {
-        stringstream str(s);
-        string nstr;
         string ans;
-        while(getline(str,nstr,' ')){
-            string temp=nstr;
-            reverse(temp.begin(),temp.end());
-            temp=temp+' ';
-            ans.append(temp);
-            
+        string temp;
+        for(int i=0;i<=s.length();i++){
+            if(s[i]!=' ' && s[i]!='\0'){
+                temp=temp+s[i];
+            }
+            else{
+                cout<<"TEMP"<<temp;
+                string nstr=reverse(temp);
+                temp.clear();
+                cout<<nstr<<endl;
+                ans=s[i]!='\0'? ans+nstr+' ':ans+nstr;
+            }
         }
-        ans.pop_back();
-        cout<<ans<<endl;
         return ans;
-        
     }
 };
