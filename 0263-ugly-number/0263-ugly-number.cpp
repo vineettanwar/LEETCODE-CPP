57b48533c4b5ab48075ffb2299ec2ee5d408d707
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2022-11-18 16:23:12 +0530

    Time: 5 ms (31.29%), Space: 5.9 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%2==0){
+            n/=2;
+        }
+        else if(n%3==0){
+            n/=3;
+        }
+        else if(n%5==0){
+            n/=5;
+        }
+        else{
+            return false;
+        }
+        return isUgly(n);
+    }
+};
