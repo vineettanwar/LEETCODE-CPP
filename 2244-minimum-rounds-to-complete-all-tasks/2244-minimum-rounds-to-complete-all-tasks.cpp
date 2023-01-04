22b53462a7e192c0bf74333d673622d5575aca2e
vineettanwar <78214514+vineettanwar@users.noreply.github.com>
2023-01-05 00:34:30 +0530

    Time: 338 ms (55.68%), Space: 103.7 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -3,12 +3,13 @@ public:
     int minimumRounds(vector<int>& tasks) {
         unordered_map<int,int> dic;
         for(auto x:tasks){
-            if(dic.find(x)!=dic.end()){
-                dic[x]++;
-            }
-            else{
-                dic[x]=1;
-            }
+            dic[x]++;
+            // if(dic.find(x)!=dic.end()){
+            //     dic[x]++;
+            // }
+            // else{
+            //     dic[x]=1;
+            // }
         }
         int count=0;
         for(auto x:dic){
